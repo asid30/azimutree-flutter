edb9298289e7eb2fae67f3935076d79b67d6c1c7
asid30 <williamcopper2003@gmail.com>
2025-04-12 14:55:31 +0700

    feat: implement sidebar widget for navigation

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:azimutree/views/pages/location_map_page.dart';
 import 'package:azimutree/views/pages/manage_data_page.dart';
 import 'package:azimutree/views/pages/scan_label_page.dart';
 import 'package:azimutree/views/pages/tutorial_page.dart';
-import 'package:azimutree/views/widgets/alert_development_widget.dart';
+import 'package:azimutree/views/widgets/sidebar_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -35,11 +35,6 @@ class _MainAppState extends State<MainApp> {
     "location_map_page": "Peta Lokasi Cluster Plot",
     "tutorial_page": "Panduan Aplikasi",
   };
-
-  void _selectPage(BuildContext context, String page) {
-    selectedPageNotifier.value = page;
-    Navigator.pop(context);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -84,71 +79,7 @@ class _MainAppState extends State<MainApp> {
             ),
           ],
         ),
-        drawer: Drawer(
-          child: ValueListenableBuilder(
-            valueListenable: selectedPageNotifier,
-            builder: (context, selectedPage, child) {
-              return ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  DrawerHeader(
-                    decoration: BoxDecoration(color: Color(0xFF1F4226)),
-                    child: Text(
-                      'Menu',
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.home),
-                    title: Text('Home'),
-                    onTap: () => _selectPage(context, "home"),
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.photo_camera),
-                    title: Text('Scan Kode Label'),
-                    onTap: () => _selectPage(context, "scan_label_page"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.storage),
-                    title: Text('Kelola Data Sampel'),
-                    onTap: () => _selectPage(context, "manage_data_page"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.map),
-                    title: Text('Peta Lokasi Cluster Plot'),
-                    onTap: () => _selectPage(context, "location_map_page"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.book),
-                    title: Text('Panduan Aplikasi'),
-                    onTap: () => _selectPage(context, "tutorial_page"),
-                  ),
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.settings),
-                    title: Text('Settings'),
-                    onTap:
-                        () => showDialog(
-                          context: context,
-                          builder: (context) => AlertDevelopmentWidget(),
-                        ),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.info),
-                    title: Text('About'),
-                    onTap:
-                        () => showDialog(
-                          context: context,
-                          builder: (context) => AlertDevelopmentWidget(),
-                        ),
-                  ),
-                  Divider(),
-                ],
-              );
-            },
-          ),
-        ),
+        drawer: SidebarWidget(),
         body: Stack(
           children: [
             //* Background App
