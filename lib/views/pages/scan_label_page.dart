054cc41e65aea9f5896847d3dc5f3e9bc1933d3d
asid30 <williamcopper2003@gmail.com>
2025-04-17 21:08:14 +0700

    feat: scan label refactor

--- lib/views/pages/scan_label_page.dart
+++ lib/views/pages/scan_label_page.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:azimutree/data/notifiers.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
@@ -13,6 +14,7 @@ class ScanLabelPage extends StatefulWidget {
 class _ScanLabelPageState extends State<ScanLabelPage> {
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
+  File? _image;
 
   @override
   void initState() {
@@ -36,32 +38,67 @@ class _ScanLabelPageState extends State<ScanLabelPage> {
       future: _initializeControllerFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
-          return Stack(
+          return Column(
             children: [
-              CameraPreview(_controller),
-              Positioned(
-                bottom: 30,
-                left: 30,
-                child: ValueListenableBuilder(
-                  valueListenable: selectedPageNotifier,
-                  builder: (context, selectedPage, child) {
-                    return ElevatedButton.icon(
-                      onPressed: () {
-                        selectedPageNotifier.value = "home";
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white,
-                        backgroundColor: const Color(0xFF1F4226),
-                        minimumSize: const Size(50, 50),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+              Stack(
+                children: [
+                  CameraPreview(_controller),
+                  Positioned(
+                    bottom: 30,
+                    left: 250,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          style: IconButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            shape: CircleBorder(),
+                          ),
+                          icon: Icon(Icons.camera, color: Colors.white),
+                          onPressed: () async {
+                            try {
+                              await _initializeControllerFuture;
+                              final _image = await _controller.takePicture();
+                              // Lakukan sesuatu dengan gambar yang diambil
+                            } catch (e) {
+                              print(e);
+                            }
+                          },
                         ),
+                        IconButton(
+                          style: IconButton.styleFrom(
+                            backgroundColor: Colors.black,
+                            shape: const CircleBorder(),
+                          ),
+                          icon: const Icon(Icons.image, color: Colors.white),
+                          onPressed: () async {
+                            // Pilih gambar dari galeri
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              ValueListenableBuilder(
+                valueListenable: selectedPageNotifier,
+                builder: (context, selectedPage, child) {
+                  return ElevatedButton.icon(
+                    onPressed: () {
+                      selectedPageNotifier.value = "home";
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color(0xFF1F4226),
+                      minimumSize: const Size(50, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      icon: const Icon(Icons.skip_previous),
-                      label: const Text("Back"),
-                    );
-                  },
-                ),
+                    ),
+                    icon: const Icon(Icons.skip_previous),
+                    label: const Text("Back"),
+                  );
+                },
               ),
             ],
           );
