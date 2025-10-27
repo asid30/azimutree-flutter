567f2c7adda56efae98c69f80ed5dcef543b081a
Abdilah Asyidiqi <70487624+asid30@users.noreply.github.com>
2025-10-27 18:40:56 +0700

    feat: update PlotClusterManageDataWidget and SelectedClusterManageDataWidget to enhance UI with Theme and improve layout

--- lib/views/widgets/plot_cluster_manage_data_widget.dart
+++ lib/views/widgets/plot_cluster_manage_data_widget.dart
@@ -20,166 +20,102 @@ class PlotClusterManageDataWidget extends StatelessWidget {
             style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
-          ExpansionTile(
-            title: Text("Plot 1"),
-            subtitle: Table(
-              columnWidths: const {
-                0: FlexColumnWidth(2),
-                1: FlexColumnWidth(3),
-              },
-              children: [
-                TableRow(children: [Text("Latitude"), Text(": -5.493510")]),
-                TableRow(children: [Text("Longitude"), Text(": 105.143400")]),
-                TableRow(children: [Text("Altitude"), Text(": 50 m")]),
-                TableRow(children: [Text("Total Pohon"), Text(": 25")]),
-              ],
-            ),
-            tilePadding: EdgeInsets.zero,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                spacing: 10,
+          Theme(
+            data: ThemeData().copyWith(dividerColor: Colors.transparent),
+            child: ExpansionTile(
+              title: Text("Plot 1"),
+              subtitle: Table(
+                columnWidths: const {
+                  0: FlexColumnWidth(2),
+                  1: FlexColumnWidth(3),
+                },
                 children: [
-                  IconButton(
-                    icon: Icon(Icons.travel_explore),
-                    tooltip: "Lihat di Peta",
-                    onPressed: () {},
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.add_location_alt),
-                    tooltip: "Tambah Data Baru",
-                    onPressed: () {},
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.delete, color: Colors.red),
-                    tooltip: "Hapus Data Plot",
-                    onPressed: () {},
-                  ),
+                  TableRow(children: [Text("Latitude"), Text(": -5.493510")]),
+                  TableRow(children: [Text("Longitude"), Text(": 105.143400")]),
+                  TableRow(children: [Text("Altitude"), Text(": 50 m")]),
+                  TableRow(children: [Text("Total Pohon"), Text(": 25")]),
                 ],
               ),
-              ExpansionTile(
-                title: Text("Pohon 1"),
-                subtitle: Table(
-                  columnWidths: const {
-                    0: FlexColumnWidth(2),
-                    1: FlexColumnWidth(3),
-                  },
-                  children: const [
-                    TableRow(
-                      children: [Text("Jenis Pohon"), Text(": Pohon Akasia")],
+              tilePadding: EdgeInsets.zero,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  spacing: 10,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.travel_explore),
+                      tooltip: "Lihat di Peta",
+                      onPressed: () {},
                     ),
-                    TableRow(children: [Text("Latitude"), Text(": -5.493510")]),
-                    TableRow(
-                      children: [Text("Longitude"), Text(": 105.143400")],
+                    IconButton(
+                      icon: Icon(Icons.add_location_alt),
+                      tooltip: "Tambah Data Baru",
+                      onPressed: () {},
                     ),
-                    TableRow(
-                      children: [Text("Jarak dari pusat"), Text(": 0 m")],
+                    IconButton(
+                      icon: Icon(Icons.delete, color: Colors.red),
+                      tooltip: "Hapus Data Plot",
+                      onPressed: () {},
                     ),
-                    TableRow(children: [Text("Altitude"), Text(": 50 m")]),
                   ],
                 ),
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    spacing: 10,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.travel_explore),
-                        tooltip: "Lihat di Peta",
-                        onPressed: () {},
+                ExpansionTile(
+                  title: Text("Pohon 1"),
+                  subtitle: Text("Pohon Akasia"),
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Table(
+                        columnWidths: const {
+                          0: FlexColumnWidth(2),
+                          1: FlexColumnWidth(3),
+                        },
+                        children: const [
+                          TableRow(
+                            children: [Text("Latitude"), Text(": -5.493510")],
+                          ),
+                          TableRow(
+                            children: [Text("Longitude"), Text(": 105.143400")],
+                          ),
+                          TableRow(
+                            children: [Text("Jarak dari pusat"), Text(": 0 m")],
+                          ),
+                          TableRow(
+                            children: [Text("Sudut Azimut"), Text(": 15°")],
+                          ),
+                          TableRow(
+                            children: [Text("Altitude"), Text(": 50 m")],
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        tooltip: "Edit Data Pohon",
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete, color: Colors.red),
-                        tooltip: "Hapus Data Pohon",
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              ListTile(title: Text("Pohon 2"), subtitle: Text("Pohon Pale")),
-              ListTile(title: Text("Pohon 3"), subtitle: Text("Pohon Jengkol")),
-              ListTile(title: Text("Pohon 4"), subtitle: Text("Pohon Kelapa")),
-              ListTile(title: Text("Pohon 5"), subtitle: Text("Pohon Jati")),
-            ],
-          ),
-          SizedBox(height: 8),
-          ExpansionTile(
-            title: Text("Plot 2"),
-            subtitle: Table(
-              columnWidths: const {
-                0: FlexColumnWidth(2),
-                1: FlexColumnWidth(3),
-              },
-              children: const [
-                TableRow(children: [Text("Latitude"), Text(": -5.493510")]),
-                TableRow(children: [Text("Longitude"), Text(": 105.143410")]),
-                TableRow(children: [Text("Altitude"), Text(": 50 m")]),
-                TableRow(children: [Text("Total Pohon"), Text(": 25")]),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      spacing: 10,
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.travel_explore),
+                          tooltip: "Lihat di Peta",
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          tooltip: "Edit Data Pohon",
+                          onPressed: () {},
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          tooltip: "Hapus Data Pohon",
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ],
             ),
-            tilePadding: EdgeInsets.zero,
-            children: [
-              ListTile(title: Text("Pohon 1"), subtitle: Text("Pohon Akasia")),
-              ListTile(title: Text("Pohon 2"), subtitle: Text("Pohon Pale")),
-              ListTile(title: Text("Pohon 3"), subtitle: Text("Pohon Jengkol")),
-              ListTile(title: Text("Pohon 4"), subtitle: Text("Pohon Kelapa")),
-              ListTile(title: Text("Pohon 5"), subtitle: Text("Pohon Jati")),
-            ],
           ),
           SizedBox(height: 8),
-          ExpansionTile(
-            title: Text("Plot 3"),
-            subtitle: Table(
-              columnWidths: const {
-                0: FlexColumnWidth(2),
-                1: FlexColumnWidth(3),
-              },
-              children: const [
-                TableRow(children: [Text("Latitude"), Text(": -5.493510")]),
-                TableRow(children: [Text("Longitude"), Text(": 105.143420")]),
-                TableRow(children: [Text("Altitude"), Text(": 50 m")]),
-                TableRow(children: [Text("Total Pohon"), Text(": 25")]),
-              ],
-            ),
-            tilePadding: EdgeInsets.zero,
-            children: [
-              ListTile(title: Text("Pohon 1"), subtitle: Text("Pohon Akasia")),
-              ListTile(title: Text("Pohon 2"), subtitle: Text("Pohon Pale")),
-              ListTile(title: Text("Pohon 3"), subtitle: Text("Pohon Jengkol")),
-              ListTile(title: Text("Pohon 4"), subtitle: Text("Pohon Kelapa")),
-              ListTile(title: Text("Pohon 5"), subtitle: Text("Pohon Jati")),
-            ],
-          ),
-          SizedBox(height: 8),
-          ExpansionTile(
-            title: Text("Plot 4"),
-            subtitle: Table(
-              columnWidths: const {
-                0: FlexColumnWidth(2),
-                1: FlexColumnWidth(3),
-              },
-              children: const [
-                TableRow(children: [Text("Latitude"), Text(": -5.493510")]),
-                TableRow(children: [Text("Longitude"), Text(": 105.143430")]),
-                TableRow(children: [Text("Altitude"), Text(": 50 m")]),
-                TableRow(children: [Text("Total Pohon"), Text(": 25")]),
-              ],
-            ),
-            tilePadding: EdgeInsets.zero,
-            children: [
-              ListTile(title: Text("Pohon 1"), subtitle: Text("Pohon Akasia")),
-              ListTile(title: Text("Pohon 2"), subtitle: Text("Pohon Pale")),
-              ListTile(title: Text("Pohon 3"), subtitle: Text("Pohon Jengkol")),
-              ListTile(title: Text("Pohon 4"), subtitle: Text("Pohon Kelapa")),
-              ListTile(title: Text("Pohon 5"), subtitle: Text("Pohon Jati")),
-            ],
-          ),
         ],
       ),
     );
